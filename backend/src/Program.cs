7df9a2cf9ef12fff4e3918919dcc945769daa956
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-08-26 17:27:46 -0300

    feat: organize project

--- backend/src/Program.cs
+++ backend/src/Program.cs
@@ -1,9 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using MStarSupply.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("WebApiDatabase"))
+);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
